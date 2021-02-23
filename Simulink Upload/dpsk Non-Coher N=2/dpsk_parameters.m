300d3e32db4be110859d5faa5a98a7ac76a9a1bc
Peyton Aplin <aplin.peyton@gmail.com>
2021-02-22 17:02:12 -0700

    Corrected M-ary DPSK Code with Non-Coherent Detection N=2

--- Simulink Upload/dpsk Non-Coher N=2/dpsk_parameters.m
+++ Simulink Upload/dpsk Non-Coher N=2/dpsk_parameters.m
@@ -1,15 +1,15 @@
 close all; clear all; clc;
 
-M = 2;
-phaseoffset = pi/2;
-snr = 10;
+M = 4;
+phaseoffset = 0; %Do not change phase offset
+snr = 12;
 samples = 1;
 stopTime = 1E5;
 stepSize = 1;
 
 img = sqrt(-1);
 
-for i = 1:M
+for i = 1:M+1
     phaseConstel(i) = cos(2*pi*(i-1)/M +phaseoffset) + img*sin(2*pi*(i-1)/M +phaseoffset);
 end
 
