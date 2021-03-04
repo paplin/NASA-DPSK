46f9978825f6e1fd42f9f45c00cc887cc1f3d1d1
paplin <aplin.peyton@gmail.com>
2021-03-04 16:43:26 -0700

    The N=3 NCD for M-DPSK is now working in the absence of the AWGN. The code seems to be abnormally affected by the AWGN.

--- Simulink Upload/dpsk Non-Coher N=3/dpsk_N3_parameters.m
+++ Simulink Upload/dpsk Non-Coher N=3/dpsk_N3_parameters.m
@@ -3,33 +3,33 @@ close all; clear all; clc;
 M = 4;
 N = 3;
 phaseoffset = 0;
-snr = 15;
+snr = 300;
 samples = 1;
-stopTime = 1E5;
+stopTime = 1E4;
 stepSize = 1;
+
+indexCheck = pi/2;
 
 img = sqrt(-1);
 
-for i = 1:M+1
+for i = 1:M
     bothDelTheta(i) = cos(2*pi*(i-1)/M +phaseoffset) + img*sin(2*pi*(i-1)/M +phaseoffset);
 end
 
-angleConstel = angle(bothDelTheta);
+angleConstel = mod(angle(bothDelTheta), 2*pi);
 
-for j = 1:size(angleConstel,2)
-    if angleConstel(j) < 0
-        angleConstel(j) = angleConstel(j) + (2*pi);
-    end
-end
 
 for q = 1:size(angleConstel, 2)
-    for r = 1:M+1
+    for r = 1:M
             mainMat(1,r,q) = angleConstel(r);
             mainMat(2,r,q) = angleConstel(q) - mainMat(1,r,q);
-            bothMat(1,r,q) = angleConstel(q);
-            bothMat(2,r,q) = angleConstel(q);
+            mainMat(3,r,q) = angleConstel(q);
+            preIndexMat(1,r,q) = mainMat(1,r,q)/(pi/2);
+            preIndexMat(2,r,q) = mod(mainMat(2,r,q),2*pi)*2/pi;
     end
 end
+
+indexMat = reshape(preIndexMat, 2, M^2);
 
 for in1 = 1:size(mainMat,1)
     for in2 = 1:size(mainMat, 2)
